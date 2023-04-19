34436142cf45230434e6c9c2e9e937a8ce7fc99d
tnsoftbear <i.kotlevskis@bidpath.com>
2023-04-20 00:44:52 +0300

    Dart - constant constructor for immutable object

---
+++ dart/idioms/const_construct_wrong.dart
@@ -0,0 +1,12 @@
+class Point<T> {
+  final T x;
+  final T y;
+  const Point(T x, T y)
+    : this.x = x,
+      this.y = y;
+}
+
+void main() {
+  const point = Point(1, 1);
+  const point2 = Point(DateTime.now(), DateTime.now());
+}
