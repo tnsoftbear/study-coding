26c32dff582b2cecf9ecbbf3ced11f6a302b6ed1
tnsoftbear <avi-team@inbox.lv>
2023-07-17 22:05:48 +0300

    JS: Pause/play YouTube player on page visibility change

--- javascript/own/page-visibility-api/page-visibility-checker.js
+++ javascript/own/page-visibility-api/page-visibility-checker.js
@@ -1,31 +1,25 @@
-init();
+var player;
+
+// Set the name of the hidden property and the change event for visibility
+var hidden, visibilityChange;
+if (typeof document.hidden !== "undefined") {
+  // Opera 12.10 and Firefox 18 and later support
+  hidden = "hidden";
+  visibilityChange = "visibilitychange";
+} else if (typeof document.msHidden !== "undefined") {
+  hidden = "msHidden";
+  visibilityChange = "msvisibilitychange";
+} else if (typeof document.webkitHidden !== "undefined") {
+  hidden = "webkitHidden";
+  visibilityChange = "webkitvisibilitychange";
+}
 
 function init() {
-  // Set the name of the hidden property and the change event for visibility
-  var hidden, visibilityChange;
-  if (typeof document.hidden !== "undefined") {
-    // Opera 12.10 and Firefox 18 and later support
-    hidden = "hidden";
-    visibilityChange = "visibilitychange";
-  } else if (typeof document.msHidden !== "undefined") {
-    hidden = "msHidden";
-    visibilityChange = "msvisibilitychange";
-  } else if (typeof document.webkitHidden !== "undefined") {
-    hidden = "webkitHidden";
-    visibilityChange = "webkitvisibilitychange";
-  }
+  initVideo();
+}
 
+function initVideo() {
   var videoElement = document.getElementById("videoElement");
-
-  // If the page is hidden, pause the video;
-  // if the page is shown, play the video
-  function handleVisibilityChange() {
-    if (document[hidden]) {
-      videoElement.pause();
-    } else {
-      videoElement.play();
-    }
-  }
 
   // Warn if the browser doesn't support addEventListener or the Page Visibility API
   if (
@@ -57,5 +51,40 @@ function init() {
       },
       false
     );
+  }
+}
+
+// Функция, вызываемая при загрузке API
+// The API will call this function when the page has finished downloading the JavaScript for the player API
+// https://developers.google.com/youtube/iframe_api_reference
+function onYouTubeIframeAPIReady() {
+  // Создание плеера
+  player = new YT.Player("player", {
+    events: {
+      onReady: onPlayerReady,
+      onStateChange: onPlayerStateChange,
+    },
+    videoId: "UONvpzG7yjo",
+  });
+}
+
+function onPlayerReady(event) {
+  document.getElementById("yt_status").innerHTML = "ready";
+}
+
+function onPlayerStateChange(event) {
+  document.getElementById("yt_status").innerHTML = event.data;
+  console.log(event);
+}
+
+// If the page is hidden, pause the video;
+// if the page is shown, play the video
+function handleVisibilityChange() {
+  if (document[hidden]) {
+    videoElement.pause();
+    player.pauseVideo();
+  } else {
+    videoElement.play();
+    player.playVideo();
   }
 }
