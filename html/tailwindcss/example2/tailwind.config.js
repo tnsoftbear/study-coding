2e1d562a13d5681bca2b966fea2110a8ef44ac19
tnsoftbear <avi-team@inbox.lv>
2024-02-04 12:09:22 +0200

    TailwindCSS: Examples

--- html/tailwindcss/example2/tailwind.config.js
+++ html/tailwindcss/example2/tailwind.config.js
@@ -10,6 +10,10 @@ module.exports = {
       colors: {
         'mainColorLight': "#E1EFFE",
         'contentScratchLight': "#E5E7EB",
+        'primaryColor': "rgb(191 219 254)",
+        'secondaryColor': "rgb(167 139 250)",
+        'secondaryColor2': "rgb(91 33 182)",
+        'tertiaryColor': "rgb(190 242 100)",
       },
       // fontFamily: {
       //   main: ['Roboto', 'sans-serif'],
