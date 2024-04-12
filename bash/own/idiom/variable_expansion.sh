0b0a745bc240d4e6ec32ee0304970456988927a0
tnsoftbear <avi-team@inbox.lv>
2024-04-12 03:14:05 +0300

    Bash: variable expansion examples

---
+++ bash/own/idiom/variable_expansion.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# set -x
+
+V1="c:/path/to/file.txt"
+echo ${V1#*/}
+echo ${V1##*/}
+echo ${V1%/*}
+echo ${V1%%/*}
+
+V2="c:/path/to/path.txt"
+echo ${V2/path/xxx}
+echo ${V2//path/xxx}
+echo ${V2/#c/xxx}
+echo ${V2/%txt/xxx}
