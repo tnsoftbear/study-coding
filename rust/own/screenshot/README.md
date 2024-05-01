bdd2766c79148141fa809960c6cadcd5ecb852c5
tnsoftbear <avi-team@inbox.lv>
2024-05-01 21:01:42 +0300

    Rust: screenshot maker

---
+++ rust/own/screenshot/README.md
@@ -0,0 +1,8 @@
+# Capture Screenshots
+
+Run application and press "Print Screen" button.
+
+## Links
+
+* [screenshots](https://crates.io/crates/screenshots)
+* [rdev](https://crates.io/crates/rdev), [rdev](https://github.com/Narsil/rdev) ~ listen and send events globally to keyboard and mouse
---
+++ rust/own/screenshot/src/storage_dir.rs
@@ -0,0 +1,41 @@
+use std::path::Path;
+use std::{env, fs};
+
+pub const TARGET_DIR: &str = "storage";
+
+pub fn prepare(mut screens_dir: String) -> String {
+    if screens_dir.is_empty() {
+        screens_dir = TARGET_DIR.to_string();
+    }
+    let mut storage_path_buf = env::current_dir().unwrap();
+    storage_path_buf.push(screens_dir);
+    let storage_path_string = storage_path_buf.to_str().unwrap().to_string();
+    if check_dir_exists(&storage_path_buf) {
+        println!("Screens will be saved in directory: {storage_path_string}");
+    } else {
+        if let Err(err) = fs::create_dir(storage_path_buf.clone()) {
+            panic!("ERROR: Cannot create directory for screens. Error: {err}");
+        }
+        println!("Directory for screens is created: {storage_path_string}");
+    }
+    verify_dir_ready(&storage_path_buf);
+    storage_path_string
+}
+
+fn check_dir_exists(path: &Path) -> bool {
+    fs::metadata(path).is_ok()
+}
+
+fn verify_dir_ready(path: &Path) {
+    let path_str = path.to_str().unwrap();
+    if let Ok(path_meta) = fs::metadata(path_str) {
+        assert!(
+            path_meta.is_dir(),
+            "ERROR: Screens save path ({path_str}) is not a directory"
+        );
+        assert!(
+            !path_meta.permissions().readonly(),
+            "ERROR: Screens save directory ({path_str}) is read-only"
+        );
+    }
+}
