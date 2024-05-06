26b54d25450720b95721e78e64ca0edce1fa7c09
tnsoftbear <avi-team@inbox.lv>
2024-05-06 18:28:14 +0300

    Rust: screenshotter - read screens dir from CLI

--- rust/own/screenshot/src/main.rs
+++ rust/own/screenshot/src/main.rs
@@ -8,9 +8,7 @@ use std::env;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
-    let screens_dir = args.get(1).unwrap_or(&String::new()).to_string(); // _or(&storage_dir::TARGET_DIR.to_string()).to_string();
-    let storage_root_path = path::storage_dir::prepare(screens_dir);
+    let screens_dir = args.get(1).unwrap_or(&String::new()).to_string();
+    let storage_root_path = path::storage_dir::determine_root_path(&screens_dir);
     keyboard::bind_print_screen(&storage_root_path);
 }
-
-
--- rust/own/screenshot/src/path/storage_dir.rs
+++ rust/own/screenshot/src/path/storage_dir.rs
@@ -1,41 +1,80 @@
-use std::path::Path;
+use std::path::{Path, PathBuf};
 use std::{env, fs};
+use std::io;
+use std::io::Write;
 
-const TARGET_DIR_DEF: &str = "storage";
+const SCREENS_DIR_DEF: &str = "storage";
 
-pub fn prepare(mut screens_dir: String) -> String {
-    if screens_dir.is_empty() {
-        screens_dir = TARGET_DIR_DEF.to_string();
+pub fn determine_root_path(screens_dir_suggestion: &str) -> String {
+    let mut screens_dir: String = screens_dir_suggestion.to_string();
+    loop {
+        match do_prepare(&screens_dir) {
+            Ok(root_path) => {
+                return root_path;
+            },
+            Err(err) => {
+                println!("ERROR: Cannot create directory for screens. {err}");
+                let screens_root_path_def = make_storage_root_path_string(SCREENS_DIR_DEF);
+                print!("Provide screens directory (Press <Enter> for default: \"{screens_root_path_def}\"): ");
+                io::stdout().flush().expect("ERROR: Unable to flush output buffer.");
+                let mut screens_dir_input: String = String::new();
+                io::stdin()
+                    .read_line(&mut screens_dir_input)
+                    .expect("ERROR: on reading directory from command line");
+                screens_dir = screens_dir_input.trim().to_string();
+            }
+        }
     }
+}
+
+fn make_storage_root_path_buf(screens_dir: &str) -> PathBuf {
     let mut storage_path_buf = env::current_dir().unwrap();
     storage_path_buf.push(screens_dir);
+    storage_path_buf
+}
+
+fn make_storage_root_path_string(screens_dir: &str) -> String {
+    let storage_path_buf = make_storage_root_path_buf(screens_dir);
     let storage_path_string = storage_path_buf.to_str().unwrap().to_string();
-    if check_dir_exists(&storage_path_buf) {
-        println!("Screens will be saved in directory: {storage_path_string}");
-    } else {
-        if let Err(err) = fs::create_dir(storage_path_buf.clone()) {
-            panic!("ERROR: Cannot create directory for screens. Error: {err}");
-        }
-        println!("Directory for screens is created: {storage_path_string}");
-    }
-    verify_dir_ready(&storage_path_buf);
     storage_path_string
 }
 
-fn check_dir_exists(path: &Path) -> bool {
+fn do_prepare(input_screens_dir: &str) -> Result<String, String> {
+    let mut screens_dir: &str = SCREENS_DIR_DEF;
+    if !input_screens_dir.is_empty() {
+        screens_dir = input_screens_dir;
+    }
+    let storage_path_buf = make_storage_root_path_buf(screens_dir);
+    let storage_path_string = make_storage_root_path_string(screens_dir);
+
+    if check_path_exists(&storage_path_buf) {
+        if let Err(error_message) = check_dir_ready(&storage_path_buf) {
+            return Err(error_message);
+        }
+        println!("Screens save path is: {storage_path_string}");
+    } else {
+        if let Err(err) = fs::create_dir(storage_path_buf.clone()) {
+            return Err(format!("{err}"));
+        }
+        println!("Directory for screens is created: {storage_path_string}");
+    }
+
+    return Ok(storage_path_string)
+}
+
+fn check_path_exists(path: &Path) -> bool {
     fs::metadata(path).is_ok()
 }
 
-fn verify_dir_ready(path: &Path) {
+fn check_dir_ready(path: &Path) -> Result<(), String> {
     let path_str = path.to_str().unwrap();
     if let Ok(path_meta) = fs::metadata(path_str) {
-        assert!(
-            path_meta.is_dir(),
-            "ERROR: Screens save path ({path_str}) is not a directory"
-        );
-        assert!(
-            !path_meta.permissions().readonly(),
-            "ERROR: Screens save directory ({path_str}) is read-only"
-        );
+        if !path_meta.is_dir() {
+            return Err(format!("Screens save path \"{path_str}\" is not a directory"));
+        }
+        if path_meta.permissions().readonly() {
+            return Err(format!("Screens save directory \"{path_str}\" is read-only"));
+        }
     }
+    Ok(())
 }
