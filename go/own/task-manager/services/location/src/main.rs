a4a54631c43d85703a195db808f0b3bdb88f744f
tnsoftbear <avi-team@inbox.lv>
2024-06-27 14:39:25 +0300

    Go: tm - fix fetching of coord in parcel locker api

--- go/own/task-manager/services/location/src/main.rs
+++ go/own/task-manager/services/location/src/main.rs
@@ -3,6 +3,7 @@
 extern crate redis;
 
 use std::collections::HashMap;
+use std::string::ToString;
 use redis::{Commands, RedisError, RedisResult};
 use redis::geo::{RadiusOptions, RadiusOrder, RadiusSearchResult, Unit};
 use warp::{Filter, Rejection, Reply};
@@ -10,45 +11,62 @@ use serde::{Deserialize, Serialize};
 use warp::http::{StatusCode};
 
 #[derive(Debug)]
-struct InvalidParameter {
-    message: String,
+enum BadRequestError {
+    ParameterNotNumeric(String),
+    ParameterRequired(String),
 }
 
-impl warp::reject::Reject for InvalidParameter {}
+impl warp::reject::Reject for BadRequestError {}
+
+impl std::fmt::Display for BadRequestError {
+    fn fmt(&self, f: &mut std::fmt::Formatter) -> std::fmt::Result {
+        match *self {
+            BadRequestError::ParameterNotNumeric(ref param) => write!(f, "{}", format!("\"{}\" parameter must be numeric value", param)),
+            BadRequestError::ParameterRequired(ref param) => write!(f, "{}", format!("\"{}\" parameter required", param)),
+        }
+    }
+}
 
 #[derive(Debug)]
 struct RedisErrorType(RedisError);
 
 impl warp::reject::Reject for RedisErrorType {}
 
-#[derive(Debug)]
-struct ParameterRequired {
-    message: String,
-}
-
-impl warp::reject::Reject for ParameterRequired {}
-
 async fn handle_rejection(err: Rejection) -> Result<impl Reply, std::convert::Infallible> {
+    #[derive(Serialize)]
+    struct Response {
+        error: String,
+    }
+
+    let error_message;
+    let status_code;
     if err.is_not_found() {
-        Ok(warp::reply::with_status("404 page not found".to_string(), StatusCode::NOT_FOUND))
-    } else if let Some(err) = err.find::<InvalidParameter>() {
-        Ok(warp::reply::with_status(err.message.clone(), StatusCode::BAD_REQUEST))
-    } else if let Some(err) = err.find::<ParameterRequired>() {
-        Ok(warp::reply::with_status(err.message.clone(), StatusCode::BAD_REQUEST))
+        error_message = "404 page not found".to_string();
+        status_code = StatusCode::NOT_FOUND;
+    } else if let Some(err) = err.find::<BadRequestError>() {
+        error_message = err.to_string();
+        status_code = StatusCode::BAD_REQUEST;
     } else if let Some(err) = err.find::<RedisErrorType>() {
         let code = err.0.code().unwrap_or("");
         let category = err.0.category();
         let message = err.0.to_string();
-        let status = format!("REDIS_ERROR: code: {}, category: {}, message: {}", code, category, message);
-        Ok(warp::reply::with_status(status, StatusCode::INTERNAL_SERVER_ERROR))
+        let status = format!("REDIS_ERROR: {} (code: {}, category: {})", message, code, category);
+        error_message = status;
+        status_code = StatusCode::INTERNAL_SERVER_ERROR;
     } else if let Some(_) = err.find::<warp::reject::MethodNotAllowed>() {
         // Silly hack for converting 405 -> 404
         // https://github.com/seanmonstar/warp/issues/77
-        Ok(warp::reply::with_status("404 page not found".to_string(), StatusCode::NOT_FOUND))
+        error_message = "404 page not found".to_string();
+        status_code = StatusCode::NOT_FOUND;
     } else {
         eprintln!("unhandled rejection: {:?}", err);
-        Ok(warp::reply::with_status("INTERNAL_SERVER_ERROR".to_string(), StatusCode::INTERNAL_SERVER_ERROR))
+        error_message = "INTERNAL_SERVER_ERROR".to_string();
+        status_code = StatusCode::INTERNAL_SERVER_ERROR;
     }
+    Ok(warp::reply::with_status(
+        warp::reply::json(&Response { error: error_message }),
+        status_code
+    ))
 }
 
 async fn ping_handler() -> Result<impl Reply, Rejection> {
@@ -111,12 +129,12 @@ async fn find_parcel_lockers_by_distance(params: HashMap<String, String>) -> Res
             match longitude.parse::<f64>() {
                 Ok(longitude) => longitude,
                 Err(_) => return Err(warp::reject::custom(
-                    InvalidParameter{message: "\"longitude\" parameter must be numeric value".to_string()}
+                    BadRequestError::ParameterNotNumeric("longitude".to_string())
                 ))
             }
         },
         None => return Err(warp::reject::custom(
-            ParameterRequired{message: "\"longitude\" parameter required".to_string()}
+            BadRequestError::ParameterRequired("longitude".to_string())
         ))
     };
     let latitude = match params.get("latitude") {
@@ -124,12 +142,12 @@ async fn find_parcel_lockers_by_distance(params: HashMap<String, String>) -> Res
             match latitude.parse::<f64>() {
                 Ok(latitude) => latitude,
                 Err(_) => return Err(warp::reject::custom(
-                    InvalidParameter{message: "\"latitude\" parameter must be numeric value".to_string()}
+                    BadRequestError::ParameterNotNumeric("latitude".to_string())
                 ))
             }
         },
         None => return Err(warp::reject::custom(
-            ParameterRequired{message: "\"latitude\" parameter required".to_string()}
+            BadRequestError::ParameterRequired("latitude".to_string())
         ))
     };
     let radius = match params.get("radius") {
@@ -137,25 +155,18 @@ async fn find_parcel_lockers_by_distance(params: HashMap<String, String>) -> Res
             match radius.parse::<f64>() {
                 Ok(radius) => radius,
                 Err(_) => return Err(warp::reject::custom(
-                    InvalidParameter{message: "\"radius\" parameter must be numeric value".to_string()}
+                    BadRequestError::ParameterNotNumeric("radius".to_string())
                 ))
             }
         },
         None => return Err(warp::reject::custom(
-            ParameterRequired{message: "\"radius\" parameter required".to_string()}
+            BadRequestError::ParameterRequired("radius".to_string())
         ))
     };
 
     #[derive(Serialize)]
-    pub struct CoordSerializable {
-        latitude: f64,
-        longitude: f64,
-    }
-
-    #[derive(Serialize)]
     pub struct RadiusSearchResultSerializable {
         name: String,
-        // coord: Option<CoordSerializable>,
         latitude: f64,
         longitude: f64,
         distance: Option<f64>,
@@ -163,27 +174,17 @@ async fn find_parcel_lockers_by_distance(params: HashMap<String, String>) -> Res
 
     impl From<&RadiusSearchResult> for RadiusSearchResultSerializable {
         fn from(rsr: &RadiusSearchResult) -> Self {
-            let mut rsrs = RadiusSearchResultSerializable {
+            RadiusSearchResultSerializable {
                 name: rsr.name.clone(),
-                // coord: rsr.coord.as_ref().map(|c| CoordSerializable {
-                //     latitude: c.latitude,
-                //     longitude: c.longitude,
-                // }),
-                latitude: 0f64,
-                longitude: 0f64,
+                latitude: rsr.coord.as_ref().unwrap().latitude,
+                longitude: rsr.coord.as_ref().unwrap().longitude,
                 distance: rsr.dist.clone(),
-            };
-            // TODO: Разобраться, почему пустые координаты
-            if let Some(coord) = rsr.coord.as_ref() {
-                rsrs.latitude = coord.latitude;
-                rsrs.longitude = coord.longitude;
             }
-            rsrs
         }
     }
 
     let mut con = connect();
-    let opts = RadiusOptions::default().with_dist().order(RadiusOrder::Asc);
+    let opts = RadiusOptions::default().with_dist().with_coord().order(RadiusOrder::Asc);
     let result: RedisResult<Vec<RadiusSearchResult>> = con.geo_radius("parcel_lockers", longitude, latitude, radius, Unit::Kilometers, opts);
     return match result {
         Ok(search_results) => {
@@ -260,6 +261,25 @@ async fn delete_parcel_locker_by_id(id: String) -> Result<impl Reply, Rejection>
     }
 }
 
+async fn delete_all_parcel_lockers() -> Result<impl Reply, Rejection> {
+    #[derive(Serialize)]
+    struct Response {
+        message: String,
+    }
+    let mut con = connect();
+    match con.zrange::<&str, Vec<String>>("parcel_lockers", 0, -1) {
+        Ok(pl_ids) => {
+            for id in pl_ids {
+                if let Err(err) = con.del::<String, ()>(make_parcel_locker_key(&id)) {
+                    return Err(warp::reject::custom(RedisErrorType(err)))
+                }
+            }
+            Ok(warp::reply::json(&Response { message: "Parcel lockers deleted".to_string() }))
+        }
+        Err(err) => Err(warp::reject::custom(RedisErrorType(err)))
+    }
+}
+
 fn make_parcel_locker_key(id: &str) -> String {
     format!("parcel_locker:{}", id)
 }
@@ -276,8 +296,7 @@ async fn main() {
         .and_then(load_parcel_locker_by_id);
 
     let get_parcel_lockers_by_distance_route = warp::get()
-        .and(warp::path("parcel-locker"))
-        .and(warp::path("distance-search"))
+        .and(warp::path("parcel-locker-distance-search"))
         .and(warp::path::end())
         .and(warp::query())
         .and_then(find_parcel_lockers_by_distance);
@@ -294,11 +313,17 @@ async fn main() {
         .and(warp::path::end())
         .and_then(delete_parcel_locker_by_id);
 
+    let delete_all_parcel_lockers_route = warp::delete()
+        .and(warp::path("all-parcel-lockers"))
+        .and(warp::path::end())
+        .and_then(delete_all_parcel_lockers);
+
     let routes = ping_route
-        .or(get_parcel_lockers_by_distance_route)
         .or(get_parcel_locker_by_id_route)
         .or(post_parcel_locker_route)
         .or(delete_parcel_locker_by_id_route)
+        .or(get_parcel_lockers_by_distance_route)
+        .or(delete_all_parcel_lockers_route)
         .recover(handle_rejection);
 
     warp::serve(routes)
