c20984c8e34dbd732859c435276258dd6c0c2cb5
tnsoftbear <avi-team@inbox.lv>
2024-07-06 17:46:40 +0300

    Rust: calc - add logging

--- rust/own/calc/src/main.rs
+++ rust/own/calc/src/main.rs
@@ -19,7 +19,8 @@ impl InvalidParam {
     }
 }
 
-async fn sum_handler(first_param: String, second_param: String) -> Result<impl Reply, Rejection> {
+async fn sum_handler(first_param: String, second_param: String, request_id: String) -> Result<impl Reply, Rejection> {
+    log::info!("request id: {request_id}");
     match first_param.parse::<i32>() {
         Ok(first_num) => {
             match second_param.parse::<i32>() {
@@ -66,19 +67,38 @@ async fn return_error(r: Rejection) -> Result<impl Reply, Rejection> {
 
 #[tokio::main]
 async fn main() {
-    let cors = warp::cors()
-        .allow_any_origin()
-        .allow_header("content-type")
-        .allow_methods(&[Method::PUT, Method::DELETE, Method::GET, Method::POST]);
+    log4rs::init_file("config/log4rs.yaml", Default::default()).unwrap();
+
+    let id_filter = warp::any().map(|| uuid::Uuid::new_v4().to_string());
 
     let sum_route = warp::path("sum")
         .and(warp::path::param())
         .and(warp::path::param())
         .and(warp::path::end())
+        .and(id_filter)
         .and_then(sum_handler)
         .recover(return_error);
 
-    let routes = sum_route.with(cors);
+    let cors = warp::cors()
+        .allow_any_origin()
+        .allow_header("content-type")
+        .allow_methods(&[Method::PUT, Method::DELETE, Method::GET, Method::POST]);
+
+    let log = warp::log::custom(|info| {
+        eprintln!(
+            "{} {} {} {:?} from {} with {:?}",
+            info.method(),
+            info.path(),
+            info.status(),
+            info.elapsed(),
+            info.remote_addr().unwrap(),
+            info.request_headers()
+        )
+    });
+
+    let routes = sum_route
+        .with(cors)
+        .with(log);
 
     warp::serve(routes)
         .run(([127, 0, 0, 1], 8080))
